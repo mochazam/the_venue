c92383aee9e9ea7d96f350e2eedc483a6e9b311f
zamroni <zamroni666@gmail.com>
2019-10-04 16:02:01 +0700

    komit pertama

---
+++ src/components/location/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Location = () => {
+    return (
+      <div className="location_wrapper">
+        
+        <iframe
+            src=""
+            width="100%"
+            height="500px"
+            frameBorder="0"
+            allowFullScreen
+        ></iframe>
+
+        <div className="location_tag">
+            <div>Location</div>
+        </div>
+
+      </div>
+    )
+}
+
+export default Location;
